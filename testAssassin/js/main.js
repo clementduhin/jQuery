e43849541ec51182f596d4803e91e89d41b6188d
clementduhin <c.duhin@gmail.com>
2020-09-24 14:11:49 +0200

    updated

--- testAssassin/js/main.js
+++ testAssassin/js/main.js
@@ -5,3 +5,22 @@ $(".panneau").click(function () {
 $(".panneau").hover(function () {
   $(this).toggleClass("panneau-survol");
 });
+
+$(document).scroll(function () {
+  Scroll1();
+  Scroll2();
+});
+
+function Scroll1() {
+  if ($(document).scrollTop() >= $(".articles1").height()) {
+    $(".article1").addClass("apparition");
+    $(".article2").addClass("apparition");
+  }
+}
+
+function Scroll2() {
+  if ($(document).scrollTop() >= $(".articles2").height() + $(".articles1").height()) {
+    $(".article3").addClass("apparition");
+    $(".article4").addClass("apparition");
+  }
+}
